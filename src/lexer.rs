03150466ceae386145f451376c0eb2915e1f9e8a
MaxXSoft <x@MaxXSoft.net>
2022-10-02 19:15:31 +0800

    (lexer) added more methods to trait `Lexer`

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,33 @@
-use crate::span::Error;
+use crate::span::{Error, Location};
 
 /// Trait for lexers.
 pub trait Lexer {
   /// Reads the next character from the input stream.
   ///
-  /// Returns the character if success,
+  /// Returns the character and the last location (location before reading
+  /// the character) if successful, or <code>[Ok]&#40;[None]&#41;</code>
+  /// if EOF was encountered, or [`Err`] if something wrong.
+  fn next_char_loc(&mut self) -> Result<(Option<char>, Location), Error>;
+
+  /// Unreads the given character and the last location
+  /// and put it back to the input stream.
+  fn unread(&mut self, last: (Option<char>, Location));
+
+  /// Reads the next character from the input stream.
+  ///
+  /// Returns the character if successful,
   /// or <code>[Ok]&#40;[None]&#41;</code> if EOF was encountered,
   /// or [`Err`] if something wrong.
-  fn next_char(&mut self) -> Result<Option<char>, Error>;
+  fn next_char(&mut self) -> Result<Option<char>, Error> {
+    self.next_char_loc().map(|char_loc| char_loc.0)
+  }
+
+  /// Peeks the next character from the input stream.
+  ///
+  /// Does not advance the position of the input stream.
+  fn peek(&mut self) -> Result<Option<char>, Error> {
+    let char_loc = self.next_char_loc()?;
+    self.unread(char_loc);
+    Ok(char_loc.0)
+  }
 }
