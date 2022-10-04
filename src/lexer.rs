548ae5d8f8e49b4100c2c92fb74b986761065923
MaxXSoft <x@MaxXSoft.net>
2022-10-04 10:37:39 +0800

    (lexer) implemented method `next_char_literal`

--- src/lexer.rs
+++ src/lexer.rs
@@ -8,9 +8,23 @@ pub trait Token<T> {
 }
 
 /// Logs error and skip until a whitespace character is encountered.
-macro_rules! log_err_and_skip {
+macro_rules! err_and_skip {
   ($self:expr, $span:expr, $($arg:tt)+) => {{
-    $self.skip_until(|c| !c.is_whitespace())?;
+    $self.skip_until(|c| c.is_whitespace())?;
+    return_error!($span, $($arg)+)
+  }};
+}
+
+/// Logs error and skip until a newline is encountered.
+macro_rules! err_and_skip_until_nl {
+  ($self:expr, $span:expr, $c:expr, $($arg:tt)+) => {{
+    if $c != Some('\n') {
+      $self.skip_until(|c| c == '\n')?;
+    }
+    return_error!($span, $($arg)+)
+  }};
+  ($self:expr, $span:expr, $($arg:tt)+) => {{
+    $self.skip_until(|c| c == '\n')?;
     return_error!($span, $($arg)+)
   }};
 }
@@ -37,7 +51,25 @@ pub trait Lexer {
   /// or <code>[Ok]&#40;[None]&#41;</code> if EOF was encountered,
   /// or [`Err`] if something wrong.
   fn next_char(&mut self) -> Result<Option<char>, Error> {
-    self.next_char_loc().map(|char_loc| char_loc.0)
+    self.next_char_loc().map(|(c, _)| c)
+  }
+
+  /// Reads the next character from the input stream.
+  ///
+  /// Returns the character and its span if successful,
+  /// or <code>[Ok]&#40;([None], _)&#41;</code> if EOF was encountered,
+  /// or [`Err`] if something wrong.
+  fn next_char_span(&mut self) -> Result<(Option<char>, Span), Error> {
+    self.next_char_loc().map(|(c, _)| (c, self.span().clone()))
+  }
+
+  /// Reads the next character from the input stream.
+  ///
+  /// Returns a reference to the span of the read character if successful,
+  /// or [`Err`] if something wrong.
+  fn next_span(&mut self) -> Result<&Span, Error> {
+    self.next_char_loc()?;
+    Ok(self.span())
   }
 
   /// Peeks the next character from the input stream.
@@ -94,10 +126,10 @@ pub trait Lexer {
     let radix = if is_zero {
       // check the next character
       let radix = match self.peek()? {
-        Some(c) if "box".contains(c) => {
+        Some(c) if "box".contains(c.to_ascii_lowercase()) => {
           // radix prefix
           int.clear();
-          Some(match c {
+          Some(match c.to_ascii_lowercase() {
             'b' => 2,
             'o' => 8,
             'x' => 16,
@@ -113,11 +145,10 @@ pub trait Lexer {
         _ => return Ok(T::new(0, span)),
       };
       // eat the current character and update the span
-      self.next_char()?;
-      span.update_end(self.span());
+      span.update_end(self.next_span()?);
       match radix {
         Some(r) => r,
-        _ => log_err_and_skip!(self, span, "invalid integer literal '{int}'"),
+        _ => err_and_skip!(self, span, "invalid integer literal '{int}'"),
       }
     } else {
       // previous digit is not zero, just a decimal
@@ -129,13 +160,12 @@ pub trait Lexer {
         break;
       }
       int.push(c);
-      self.next_char()?;
-      span.update_end(self.span());
+      span.update_end(self.next_span()?);
     }
     // convert to integer
     match u64::from_str_radix(&int, radix) {
       Ok(i) => Ok(T::new(i, span)),
-      _ => log_err_and_skip!(self, span, "invalid integer literal '{int}'"),
+      _ => err_and_skip!(self, span, "invalid integer literal '{int}'"),
     }
   }
 
@@ -187,6 +217,12 @@ pub trait Lexer {
 
   /// Reads the next string literal (`"..."`) from the input stream.
   ///
+  /// Supported escapes:
+  /// * `\r`, `\n`, `\t`, `\0`, `\\`.
+  /// * `\'`, `\"`.
+  /// * `\x00`-`\xFF` (`\xff`).
+  /// * `\u{0}`-`\u{10ffff}` (`\u{10FFFF}`).
+  ///
   /// Returns the token if successful, otherwise returns [`Err`]
   /// and skips until a whitespace character is encountered.
   fn next_str<T>(&mut self) -> Result<T, Error>
@@ -198,13 +234,111 @@ pub trait Lexer {
 
   /// Reads the next character literal (`'...'`) from the input stream.
   ///
+  /// Supported escapes:
+  /// * `\r`, `\n`, `\t`, `\0`, `\\`.
+  /// * `\'`, `\"`.
+  /// * `\x00`-`\xFF` (`\xff`).
+  /// * `\u{0}`-`\u{10ffff}` (`\u{10FFFF}`).
+  ///
   /// Returns the token if successful, otherwise returns [`Err`]
   /// and skips until a whitespace character is encountered.
   fn next_char_literal<T>(&mut self) -> Result<T, Error>
   where
     T: Token<char>,
   {
-    todo!()
+    // check and skip the first character
+    let span = match self.peek_with_span()? {
+      (Some('\''), span) => {
+        self.next_char()?;
+        span
+      }
+      (_, span) => return_error!(span, "invalid character literal"),
+    };
+    // check the next character
+    let c = match self.next_char_span()? {
+      (Some('\''), span) => return_error!(span, "character literal must not be empty"),
+      (c @ (Some('\n') | Some('\r') | Some('\t')), span) => {
+        let escaped = match c {
+          Some('\n') => "\\n",
+          Some('\r') => "\\r",
+          Some('\t') => "\\t",
+          _ => unreachable!(),
+        };
+        err_and_skip_until_nl!(self, span, c, "character should be escaped to '{escaped}'");
+      }
+      (Some('\\'), span) => {
+        let (c, sp) = self.next_char_span()?;
+        let span = span.into_end_updated(sp);
+        match c {
+          Some('r') => '\r',
+          Some('n') => '\n',
+          Some('t') => '\t',
+          Some('0') => '\0',
+          Some('\\') => '\\',
+          Some('x') => {
+            // get escaped char
+            let c = self
+              .next_char()?
+              .zip(self.peek()?)
+              .and_then(|(c1, c2)| u8::from_str_radix(&format!("{c1}{c2}"), 16).ok())
+              .map(|b| b as char);
+            // get the last char
+            let last_char = self.next_char()?;
+            let span = span.into_end_updated(self.span());
+            match c {
+              Some(c) => c,
+              None => err_and_skip_until_nl!(self, span, last_char, "invalid byte escape"),
+            }
+          }
+          Some('u') => {
+            // check '{'
+            match self.next_char_span()? {
+              (Some(c), _) if c == '{' => {}
+              (c, span) => err_and_skip_until_nl!(self, span, c, "expected '{{'"),
+            }
+            // get hex value
+            let mut hex = String::new();
+            while let Some(c) = self.peek()? {
+              if !c.is_ascii_hexdigit() {
+                break;
+              }
+              hex.push(c);
+              self.next_char()?;
+            }
+            // check and eat '}'
+            match self.next_char_span()? {
+              (Some('}'), sp) => {
+                let span = span.into_end_updated(sp);
+                // parse the hex value
+                let hex = match u32::from_str_radix(&hex, 16) {
+                  Ok(hex) => hex,
+                  _ => err_and_skip_until_nl!(self, span, "invalid hexadecimal value '{hex}'"),
+                };
+                // convert to unicode character
+                match char::from_u32(hex) {
+                  Some(c) => c,
+                  None => {
+                    err_and_skip_until_nl!(self, span, "invalid unicode escape '\\u{{{hex}}}'")
+                  }
+                }
+              }
+              (c, span) => err_and_skip_until_nl!(self, span, c, "expected '}}'"),
+            }
+          }
+          _ => err_and_skip_until_nl!(self, span, c, "unknown escape"),
+        }
+      }
+      (Some(c), _) => c,
+      (None, span) => return_error!(span, "unterminated character literal"),
+    };
+    // check and eat the quote
+    match self.peek_with_span()? {
+      (Some('\''), s) => {
+        self.next_char()?;
+        Ok(T::new(c, span.into_end_updated(s)))
+      }
+      (_, span) => err_and_skip_until_nl!(self, span, "expected quote (')"),
+    }
   }
 }
 
