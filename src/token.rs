aa98e171b9d23e4d42b446d7aaf9b07ebbb3dda0
MaxXSoft <x@MaxXSoft.net>
2022-10-16 18:50:44 +0800

    (token) added more methods for trait `TokenStream`

--- src/token.rs
+++ src/token.rs
@@ -15,4 +15,50 @@ pub trait TokenStream {
   ///
   /// Returns the token if successful, otherwise [`Err`].
   fn next_token(&mut self) -> Result<Self::Token, Error>;
+
+  /// Unreads the given token and put it back to the token stream.
+  fn unread(&mut self, token: Self::Token);
+
+  /// Peeks the next character from the input stream.
+  ///
+  /// Does not advance the position of the token stream.
+  fn peek(&mut self) -> Result<Self::Token, Error>
+  where
+    Self::Token: Clone,
+  {
+    let token = self.next_token()?;
+    self.unread(token.clone());
+    Ok(token)
+  }
+
+  /// Skips tokens untils a token specified by the predicate is encountered.
+  fn skip_until<F>(&mut self, mut f: F) -> Result<(), Error>
+  where
+    F: FnMut(&Self::Token) -> bool,
+  {
+    loop {
+      let token = self.next_token()?;
+      if f(&token) {
+        self.unread(token);
+        break Ok(());
+      }
+    }
+  }
+
+  /// Collects tokens into a [`Vec`] until a token specified by the predicate
+  /// is encountered.
+  fn collect_until<F>(&mut self, mut f: F) -> Result<Vec<Self::Token>, Error>
+  where
+    F: FnMut(&Self::Token) -> bool,
+  {
+    let mut v = Vec::new();
+    loop {
+      let token = self.next_token()?;
+      if f(&token) {
+        self.unread(token);
+        break Ok(v);
+      }
+      v.push(token);
+    }
+  }
 }
