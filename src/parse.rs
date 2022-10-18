03bca9cd6b1afb0843cf5f1b067e1d7d786fc27f
MaxXSoft <x@MaxXSoft.net>
2022-10-18 11:02:09 +0800

    (parse) implemented trait `Parse` for `Option`

--- src/parse.rs
+++ src/parse.rs
@@ -9,7 +9,7 @@ where
   /// Parses a instance of the current type from the given token stream.
   fn parse(tokens: &mut TS) -> Result<Self>;
 
-  /// Checks if the current type can be parsed from the given token stream.
+  /// Checks if the current type may be parsed from the given token stream.
   ///
   /// Does not advance the position of the token stream.
   fn maybe(tokens: &mut TS) -> Result<bool>;
@@ -26,5 +26,19 @@ where
 
   fn maybe(tokens: &mut TS) -> Result<bool> {
     T::maybe(tokens)
+  }
+}
+
+impl<TS, T> Parse<TS> for Option<T>
+where
+  TS: TokenStream,
+  T: Parse<TS>,
+{
+  fn parse(tokens: &mut TS) -> Result<Self> {
+    T::maybe(tokens)?.then(|| T::parse(tokens)).transpose()
+  }
+
+  fn maybe(_: &mut TS) -> Result<bool> {
+    Ok(true)
   }
 }
