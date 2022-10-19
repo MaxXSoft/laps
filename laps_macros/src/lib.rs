63730950326f97a8f5fd752249bb30f5db5c086b
MaxXSoft <x@MaxXSoft.net>
2022-10-19 20:12:29 +0800

    (lib) changed attribute `custom` to `maybe`

--- laps_macros/src/lib.rs
+++ laps_macros/src/lib.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Parse, attributes(token_stream, custom))]
+#[proc_macro_derive(Parse, attributes(token_stream, maybe))]
 pub fn derive_parse(tokens: TokenStream) -> TokenStream {
   match parse::derive_parse(tokens) {
     Ok(data) => data,
