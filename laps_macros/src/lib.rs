7c8a8f73deb6307b13bf07146d53a779577bf284
MaxXSoft <x@MaxXSoft.net>
2022-10-19 19:04:41 +0800

    (lib) changed attribute `starts_with` to `custom`

--- laps_macros/src/lib.rs
+++ laps_macros/src/lib.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Parse, attributes(token_stream, starts_with))]
+#[proc_macro_derive(Parse, attributes(token_stream, custom))]
 pub fn derive_parse(tokens: TokenStream) -> TokenStream {
   match parse::derive_parse(tokens) {
     Ok(data) => data,
