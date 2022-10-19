1f6281c03ba79522031316617634cedc34e90d9e
MaxXSoft <x@MaxXSoft.net>
2022-10-19 17:28:30 +0800

    (utils) added more utilities

--- laps_macros/src/utils.rs
+++ laps_macros/src/utils.rs
@@ -1,6 +1,7 @@
 use proc_macro2::{Ident, Span};
 use syn::parse::{Parse, ParseStream};
-use syn::{parenthesized, Result};
+use syn::punctuated::Punctuated;
+use syn::{parenthesized, Result, Token};
 
 /// Generates a compile error.
 macro_rules! error {
@@ -38,4 +39,30 @@ impl<T: Parse> Parse for Parenthesized<T> {
 /// Creates a new identifier by the given string.
 pub fn ident(s: &str) -> Ident {
   Ident::new(s, Span::call_site())
+}
+
+/// Data of `key = value`.
+pub struct KeyValue<S, V> {
+  pub key: Ident,
+  pub separator: S,
+  pub value: V,
+}
+
+impl<S: Parse, V: Parse> Parse for KeyValue<S, V> {
+  fn parse(input: ParseStream) -> Result<Self> {
+    Ok(Self {
+      key: input.parse()?,
+      separator: input.parse()?,
+      value: input.parse()?,
+    })
+  }
+}
+
+/// Data of comma separated items.
+pub struct CommaSep<T>(pub Punctuated<T, Token![,]>);
+
+impl<T: Parse> Parse for CommaSep<T> {
+  fn parse(input: ParseStream) -> Result<Self> {
+    Ok(Self(Punctuated::parse_terminated(input)?))
+  }
 }
