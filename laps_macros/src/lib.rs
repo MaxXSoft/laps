5224fa09e4f0afcdc0a2a627f0cc6d4056b77f6f
MaxXSoft <x@MaxXSoft.net>
2022-10-20 09:15:19 +0800

    (macros/lib) added attribute macro `token_kind`

--- laps_macros/src/lib.rs
+++ laps_macros/src/lib.rs
@@ -1,13 +1,18 @@
 mod parse;
+mod token_kind;
 mod utils;
 
 use proc_macro::TokenStream;
+use utils::result_to_tokens;
 
 /// Generates the `Parse` implementation.
 #[proc_macro_derive(Parse, attributes(token_stream, maybe))]
 pub fn derive_parse(tokens: TokenStream) -> TokenStream {
-  match parse::derive_parse(tokens) {
-    Ok(data) => data,
-    Err(err) => err.to_compile_error().into(),
-  }
+  result_to_tokens!(parse::derive_parse(tokens))
+}
+
+/// Implements `From` and `Display` trait for token kind enums.
+#[proc_macro_attribute]
+pub fn token_kind(attr: TokenStream, item: TokenStream) -> TokenStream {
+  result_to_tokens!(token_kind::token_kind(attr, item))
 }
