ee1fc0fa010db07786d41b6d6fd7d9d171979dbc
MaxXSoft <x@MaxXSoft.net>
2022-10-25 13:17:42 +0800

    (lib) added some doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Lexer and parser collections.
+//!
+//! With `laps`, you can build parsers by just defining ASTs and deriving `Parse` trait for them.
+//!
+//! # Examples
+//!
+//! See the [`examples` directory](https://github.com/MaxXSoft/laps/tree/master/examples),
+//! which contains the following examples:
+//!
+//! * [`clike`](https://github.com/MaxXSoft/laps/tree/master/examples/clike):
+//!   interpreter for a C-like programming language, with a front-end built with `laps`.
+
 pub mod ast;
 pub mod input;
 pub mod parse;
