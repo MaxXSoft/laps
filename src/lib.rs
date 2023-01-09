1c75eeef38627f7b51692ba2b55366e225b17097
MaxXSoft <x@MaxXSoft.net>
2023-01-09 21:24:00 +0800

    (lib) updated the top-level doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
 //! Lexer and parser collections.
 //!
-//! With `laps`, you can build parsers by just defining ASTs and deriving `Parse` trait for them.
+//! With `laps`, you can build parsers by just defining ASTs and deriving
+//! [`Parse`](parse::Parse) trait for them.
 //!
-//! # Examples
+//! # Example
 //!
-//! See the [`examples` directory](https://github.com/MaxXSoft/laps/tree/master/examples),
+#![cfg_attr(not(feature = "macros"), doc = " ```ignore")]
+#![cfg_attr(feature = "macros", doc = " ```no_run")]
+#![doc = include_str!("../examples/calc/main.rs")]
+//! ```
+//!
+//! # More Examples
+//!
+//! See the
+//! [`examples` directory](https://github.com/MaxXSoft/laps/tree/master/examples),
 //! which contains the following examples:
 //!
-//! * [`clike`](https://github.com/MaxXSoft/laps/tree/master/examples/clike):
-//!   interpreter for a C-like programming language, with a front-end built with `laps`.
+//! * [`calc`](https://github.com/MaxXSoft/laps/tree/master/examples/calc):
+//!   a simple expression calculator.
 //! * [`json`](https://github.com/MaxXSoft/laps/tree/master/examples/json):
-//!   a simple JSON parser, with a front-end built with `laps`.
+//!   a simple JSON parser.
+//! * [`clike`](https://github.com/MaxXSoft/laps/tree/master/examples/clike):
+//!   interpreter for a C-like programming language.
 
 pub mod ast;
 pub mod input;
