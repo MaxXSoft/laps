2102a869d3ef44cb0be18e37dea43f4c46343723
MaxXSoft <x@MaxXSoft.net>
2023-06-17 18:24:25 +0800

    (token_ast) updated syntax for macro `token_ast`

--- laps_macros/src/token_ast.rs
+++ laps_macros/src/token_ast.rs
@@ -3,121 +3,106 @@ use proc_macro::TokenStream;
 use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::quote;
 use syn::{
-  braced, bracketed, parenthesized,
+  braced, bracketed,
   parse::{Parse, ParseStream},
-  punctuated::Punctuated,
+  punctuated::{Pair, Punctuated},
   spanned::Spanned,
-  token::{Brace, Bracket, Paren},
-  Attribute, LitStr, Pat, Path, Result, Token, Visibility,
+  Attribute, GenericArgument, LitStr, Meta, Pat, Path, PathArguments, PathSegment, Result, Token,
+  Type, Visibility,
 };
 
 struct TokenAst {
   attrs: Vec<Attribute>,
+  derives: Vec<Attribute>,
   vis: Visibility,
-  _macro_token: Token![macro],
-  ident: Ident,
-  _paren_token: Paren,
-  configs: Configs,
-  _brace_token: Brace,
+  current_mod: Path,
+  name: Ident,
+  token_kind: Type,
   arms: Punctuated<TokenAstArm, Token![,]>,
 }
 
 impl Parse for TokenAst {
   fn parse(input: ParseStream) -> Result<Self> {
-    let paren_content;
-    let brace_content;
-    Ok(Self {
-      attrs: input.call(Attribute::parse_outer)?,
-      vis: input.parse()?,
-      _macro_token: input.parse()?,
-      ident: input.parse()?,
-      _paren_token: parenthesized!(paren_content in input),
-      configs: paren_content.parse()?,
-      _brace_token: braced!(brace_content in input),
-      arms: Punctuated::parse_terminated(&brace_content)?,
-    })
-  }
-}
-
-struct Configs {
-  current_mod: Path,
-  token_kind: Path,
-  derives: TokenStream2,
-}
-
-impl Parse for Configs {
-  fn parse(input: ParseStream) -> Result<Self> {
-    // parse current module
-    input.parse::<Token![mod]>()?;
-    input.parse::<Token![=]>()?;
-    let current_mod = input.parse()?;
-    // parse token kind
-    input.parse::<Token![,]>()?;
-    let kind: Ident = input.parse()?;
-    if kind != "Kind" {
-      return_error!(kind.span(), "must be `Kind`");
-    }
-    input.parse::<Token![=]>()?;
-    let token_kind = input.parse()?;
-    // parse derive (optional)
-    let derives = if input.peek(Token![,]) && input.peek2(syn::Ident) {
-      input.parse::<Token![,]>()?;
-      let derive: Ident = input.parse()?;
-      if derive != "derive" {
-        return_error!(kind.span(), "must be `derive`");
-      }
-      input.parse::<Token![=]>()?;
-      let derives_content;
-      parenthesized!(derives_content in input);
-      derives_content.parse()?
-    } else {
-      TokenStream2::new()
+    // parse attributes and derives
+    let (derives, attrs) = input
+      .call(Attribute::parse_outer)?
+      .into_iter()
+      .partition(|attr| matches!(&attr.meta, Meta::List(l) if l.path.is_ident("derive")));
+    // parse visibility and `macro`
+    let vis = input.parse()?;
+    input.parse::<Token![macro]>()?;
+    // parse current module, name and token kind
+    let mut current_mod: Path = input.parse()?;
+    let (name, token_kind) = match current_mod.segments.pop() {
+      Some(Pair::End(PathSegment {
+        ident,
+        arguments: PathArguments::AngleBracketed(mut a),
+      })) => match a.args.pop() {
+        Some(Pair::End(GenericArgument::Type(ty))) if a.args.is_empty() => (ident, ty),
+        _ => return_error!(a.span(), "must have only one type parameter"),
+      },
+      _ => return_error!(current_mod.span(), "invalid path"),
     };
-    // parse the ending comma
-    if input.peek(Token![,]) {
-      input.parse::<Token![,]>()?;
-    }
+    // parse arms
+    let brace_content;
+    braced!(brace_content in input);
+    let arms = Punctuated::parse_terminated(&brace_content)?;
     Ok(Self {
-      current_mod,
-      token_kind,
+      attrs,
       derives,
+      vis,
+      current_mod,
+      name,
+      token_kind,
+      arms,
     })
   }
 }
 
 struct TokenAstArm {
-  _bracket_token: Bracket,
   token: TokenStream2,
-  _fat_arrow_token: Token![=>],
-  _paren_token: Paren,
   pat: Pat,
-  _comma_token: Token![,],
   prompt: Option<LitStr>,
 }
 
 impl Parse for TokenAstArm {
   fn parse(input: ParseStream) -> Result<Self> {
+    // parse token
     let bracket_content;
-    let paren_content;
-    Ok(Self {
-      _bracket_token: bracketed!(bracket_content in input),
-      token: bracket_content.parse()?,
-      _fat_arrow_token: input.parse()?,
-      _paren_token: parenthesized!(paren_content in input),
-      pat: Pat::parse_multi_with_leading_vert(&paren_content)?,
-      _comma_token: paren_content.parse()?,
-      prompt: {
-        let lookahead = paren_content.lookahead1();
-        if lookahead.peek(LitStr) {
-          Some(paren_content.parse()?)
-        } else if lookahead.peek(Token![_]) {
-          paren_content.parse::<Token![_]>()?;
-          None
-        } else {
-          return Err(lookahead.error());
-        }
-      },
-    })
+    bracketed!(bracket_content in input);
+    let token = bracket_content.parse()?;
+    // parse arm
+    input.parse::<Token![=>]>()?;
+    let brace_content;
+    braced!(brace_content in input);
+    // parse `kind:`
+    let kind: Ident = brace_content.parse()?;
+    if kind != "kind" {
+      return_error!(kind.span(), "must be `kind`");
+    }
+    brace_content.parse::<Token![:]>()?;
+    // parse pattern
+    let pat = Pat::parse_multi_with_leading_vert(&brace_content)?;
+    // parse the optional prompt part
+    let prompt = if brace_content.peek(Token![,]) && brace_content.peek2(syn::Ident) {
+      brace_content.parse::<Token![,]>()?;
+      // parse `prompt:`
+      let prompt_ident: Ident = brace_content.parse()?;
+      if prompt_ident != "prompt" {
+        return_error!(prompt_ident.span(), "must be `prompt`");
+      }
+      brace_content.parse::<Token![:]>()?;
+      // parse prompt
+      let prompt = brace_content.parse()?;
+      // parse the optional comma
+      if brace_content.peek(Token![,]) {
+        brace_content.parse::<Token![,]>()?;
+      }
+      Some(prompt)
+    } else {
+      None
+    };
+    Ok(Self { token, pat, prompt })
   }
 }
 
@@ -139,7 +124,7 @@ fn gen_ast_defs(input: &TokenAst) -> Result<(TokenStream2, Vec<TokenStream2>)> {
   // generate AST names
   let names = (0..input.arms.len()).map(|i| ident(&format!("Token{i}")));
   // generate AST definitions
-  let kind = &input.configs.token_kind;
+  let kind = &input.token_kind;
   let field_vis = match &input.vis {
     Visibility::Inherited => quote!(pub(super)),
     Visibility::Restricted(res) => {
@@ -160,11 +145,11 @@ fn gen_ast_defs(input: &TokenAst) -> Result<(TokenStream2, Vec<TokenStream2>)> {
     vis => quote!(#vis),
   };
   let token = quote!(laps::token::Token<#kind>);
-  let derive = if input.configs.derives.is_empty() {
+  let derive = if input.derives.is_empty() {
     quote!(#[derive(PartialEq)])
   } else {
-    let derives = &input.configs.derives;
-    quote!(#[derive(#derives)])
+    let derives = &input.derives;
+    quote!(#(#derives)*)
   };
   let defs = names
     .clone()
@@ -233,7 +218,7 @@ fn gen_ast_defs(input: &TokenAst) -> Result<(TokenStream2, Vec<TokenStream2>)> {
       }
     });
   let vis = &input.vis;
-  let mod_name = ident(&format!("__token_ast_{}", input.ident));
+  let mod_name = ident(&format!("__token_ast_{}", input.name));
   let ast_defs = quote! {
     #[doc(hidden)]
     #vis mod #mod_name {
@@ -242,8 +227,8 @@ fn gen_ast_defs(input: &TokenAst) -> Result<(TokenStream2, Vec<TokenStream2>)> {
     }
   };
   // generate full paths for all ASTs
-  let current_mod = &input.configs.current_mod;
-  let ast_names = names.map(|ident| quote!(#current_mod::#mod_name::#ident));
+  let current_mod = &input.current_mod;
+  let ast_names = names.map(|ident| quote!(#current_mod #mod_name::#ident));
   Ok((ast_defs, ast_names.collect()))
 }
 
@@ -256,7 +241,7 @@ fn gen_macro_def(input: &TokenAst, ast_names: Vec<TokenStream2>) -> TokenStream2
     .map(|(name, TokenAstArm { token, .. })| quote!([#token] => {#name};));
   // generate definition
   let attrs = &input.attrs;
-  let name = &input.ident;
+  let name = &input.name;
   let macro_def = quote! {
     #(#attrs)*
     macro_rules! #name {
