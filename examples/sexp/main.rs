da89f02c5d6eb22c3bf22395a6ea76dcd4d85004
MaxXSoft <x@MaxXSoft.net>
2023-06-17 18:53:43 +0800

    (examples/sexp) updated for new implementations

--- examples/sexp/main.rs
+++ examples/sexp/main.rs
@@ -3,18 +3,21 @@ use laps::{prelude::*, reader::Reader, span::Result, token::TokenBuffer};
 /// Kinds of the token.
 ///
 /// The tokenizer (lexer) will read user input and turn it into a stream of
-/// tokens.
-///
-/// In the subsequent implementation of the [`Tokenizer`] trait of [`Lexer`],
-/// the corresponding code to generate tokens based on the input is included.
+/// tokens based on regular expressions.
 #[token_kind]
-#[derive(Debug)]
+#[derive(Debug, Tokenize)]
 enum TokenKind {
-  /// Atom.
-  Atom(String),
+  // This token will be skipped.
+  #[skip(r"\s+")]
+  _Skip,
   /// Parentheses.
+  #[regex(r"[()]")]
   Paren(char),
+  /// Atom.
+  #[regex(r"[^\s()]+")]
+  Atom(String),
   /// End-of-file.
+  #[eof]
   Eof,
 }
 
@@ -25,45 +28,17 @@ enum TokenKind {
 /// the token in the input.
 type Token = laps::token::Token<TokenKind>;
 
-/// The lexer.
-///
-/// There is a [`Reader`] in the lexer in order to read characters from
-/// the input.
-struct Lexer<T>(Reader<T>);
-
-impl<T: std::io::Read> Tokenizer for Lexer<T> {
-  type Token = Token;
-
-  fn next_token(&mut self) -> Result<Self::Token> {
-    // Skip spaces.
-    self.0.skip_until(|c| !c.is_whitespace())?;
-    // Check the current character.
-    Ok(match self.0.peek()? {
-      // Parentheses.
-      Some(c) if c == '(' || c == ')' => Token::new(c, self.0.next_span()?.clone()),
-      // Atom.
-      Some(_) => {
-        let (atom, span) = self
-          .0
-          .collect_with_span_until(|c| c.is_whitespace() || c == '(' || c == ')')?;
-        Token::new(atom, span)
-      }
-      // End-of-file.
-      None => Token::new(TokenKind::Eof, self.0.next_span()?.clone()),
-    })
-  }
-}
-
 token_ast! {
   /// Macro for referencing ASTs corresponding to tokens.
   ///
   /// The [`token_ast`] macro defines ASTs for tokens, and automatically
   /// implements methods for parsing them.
-  macro Token(mod = crate, Kind = TokenKind, derive = (Clone, Debug, PartialEq)) {
-    [atom] => (TokenKind::Atom(_), "atom"),
-    [lpr] => (TokenKind::Paren('('), _),
-    [rpr] => (TokenKind::Paren(')'), _),
-    [eof] => (TokenKind::Eof, _),
+  #[derive(Clone, Debug, PartialEq)]
+  macro Token<TokenKind> {
+    [atom] => { kind: TokenKind::Atom(_), prompt: "atom" },
+    [lpr] => { kind: TokenKind::Paren('(') },
+    [rpr] => { kind: TokenKind::Paren(')') },
+    [eof] => { kind: TokenKind::Eof },
   }
 }
 
@@ -97,7 +72,7 @@ enum Elem {
 fn main() -> Result<()> {
   // Create a reader and a lexer.
   let reader = Reader::from_stdin();
-  let lexer = Lexer(reader);
+  let lexer = TokenKind::lexer(reader);
   // Create a token buffer for parsing.
   // Token buffer can temporarily hold tokens to help the parser perform
   // some look-ahead operations.
